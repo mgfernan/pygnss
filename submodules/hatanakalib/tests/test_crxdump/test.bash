786882b65fcd72b9e3b746d482bfa5148e12b641
Miquel Garcia <miquel.garcia@rokubun.cat>
2025-02-04 17:14:47 +0000

    refactor(submodules): move hatanakalib to submodules folder

--- submodules/hatanakalib/tests/test_crxdump/test.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-PROGRAM="${BIN_PATH}/crxdump"
-
-ANSWER="${TESTS_SOURCE_FOLDER}/test_crxdump/answer.txt"
-
-zcat ${TESTS_SOURCE_FOLDER}/data/ACSO00XXX_R_20241310000_05S_01S_MO.crx.gz  | ${PROGRAM} | diff - ${ANSWER}
